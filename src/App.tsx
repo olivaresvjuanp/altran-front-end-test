0e4bd9aea09f067f32571de0cbf428edfe731ee8
olivaresvjuanp <olivaresvjuanp@gmail.com>
2020-07-26 14:05:08 -0400

    feat: Install Material-UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,8 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Typography } from '@material-ui/core';
 
-function App() {
+export const App: React.FunctionComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Typography>Altran Front-end Test</Typography>
   );
-}
-
-export default App;
+};
